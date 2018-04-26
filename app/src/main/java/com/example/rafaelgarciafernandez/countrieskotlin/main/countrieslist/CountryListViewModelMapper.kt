16c831e94d63e54b8c05f18f42481dd053e36bf2
Rafagf <rafagarfer@gmail.com>
2018-04-26 21:34:08 +0900

    Mapper test written but not passing either

--- app/src/main/java/com/example/rafaelgarciafernandez/countrieskotlin/main/countrieslist/CountryListViewModelMapper.kt
+++ app/src/main/java/com/example/rafaelgarciafernandez/countrieskotlin/main/countrieslist/CountryListViewModelMapper.kt
@@ -3,6 +3,7 @@ package com.example.rafaelgarciafernandez.countrieskotlin.main.countrieslist
 import com.example.rafaelgarciafernandez.countrieskotlin.model.Country
 
 class CountryListViewModelMapper {
+
     fun mapFrom(country: Country): CountryListViewModel {
         return CountryListViewModel(country.name, country.alpha2Code, country.continent, country.population)
     }
